7ec9e328cbf65b52d0790faaa4faa463d7471a3c
Innocent Asiimwe <innocent.asiimwe@andela.com>
2019-03-17 14:42:45 +0300

    add postgres setup

--- services/users/Dockerfile-dev.Dockerfile
+++ services/users/Dockerfile-dev.Dockerfile
@@ -1,11 +1,19 @@
 FROM python:3.6-alpine
 
+RUN apk update && \
+    apk add --virtual build-deps gcc python-dev musl-dev && \
+    apk add postgresql-dev && \
+    apk add netcat-openbsd
+
 WORKDIR /usr/src/app
 
 COPY ./requirements.txt /usr/src/app/requirements.txt
 
 RUN pip install -r requirements.txt
 
+ADD ./entrypoint.sh  /usr/src/app/entrypoint.sh
+RUN chmod +x /usr/src/app/entrypoint.sh
+
 COPY . /usr/src/app
 
-CMD python manage.py run -h 0.0.0.0
+CMD ["sh", "/usr/src/app/entrypoint.sh"]
--- services/users/project/config.py
+++ services/users/project/config.py
@@ -1,15 +1,23 @@
+import os
+
+
 class BaseConfig:
     """Base configs"""
     TESTING = False
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
 
 class DevelopmentConfig(BaseConfig):
     """Development configs"""
-    pass
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
+
 
 class TestingConfig(BaseConfig):
     """Testing configs"""
     TESTING = True
+    SQLALCHECMY_DATABASE_URI = os.getenv('DATABASE_TEST_URL')
+
 
 class ProductionConfig(BaseConfig):
     """Production configuration"""
-    pass
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')

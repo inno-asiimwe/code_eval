3759431b61ca820ba7d0f1017b57cf708fdc5a9e
Innocent Asiimwe <innocent.asiimwe@andela.com>
2019-03-17 19:47:29 +0300

    Add test setup

--- services/users/manage.py
+++ services/users/manage.py
@@ -1,3 +1,4 @@
+import unittest
 from flask.cli import FlaskGroup
 
 from project import app, db
@@ -11,6 +12,15 @@ def recreate_db():
     db.create_all()
     db.session.commit()
     print("Done")
+
+
+@cli.command()
+def test():
+    tests = unittest.TestLoader().discover('project/tests', pattern='test*.py')
+    result = unittest.TextTestRunner(verbosity=2).run(tests)
+    if result.wasSuccessful():
+        return 0
+    return 1
 
 if __name__ == '__main__':
     cli()

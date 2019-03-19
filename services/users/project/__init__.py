be3c44ef54e84ce29eb1f210e3d92c7256a10056
Innocent Asiimwe <innocent.asiimwe@andela.com>
2019-03-19 15:07:55 +0300

    Setup users blueprint

--- services/users/project/__init__.py
+++ services/users/project/__init__.py
@@ -1,31 +1,26 @@
 import os
-import sys
-from flask import Flask, jsonify
+
+from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
-app = Flask(__name__)
 
-app_settings = os.getenv('APP_SETTINGS')
-app.config.from_object('project.config.DevelopmentConfig')
-
-db = SQLAlchemy(app)
+db = SQLAlchemy()
 
 
-class User(db.Model):
-    __tablename__ = 'users'
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    username = db.Column(db.String(128), nullable=False)
-    email = db.Column(db.String(128), nullable=False)
-    active = db.Column(db.Boolean(), default=True, nullable=False)
+def create_app(script_info=None):
 
-    def __init__(self, username, email):
-        self.username = username
-        self.email = email
+    app = Flask(__name__)
 
+    app_settings = os.getenv('APP_SETTINGS')
+    app.config.from_object(app_settings)
 
-@app.route('/users/ping', methods=['GET'])
-def ping_pong():
-    return jsonify({
-        'status': 'success',
-        'message': 'pong'
-    })
+    db.init_app(app)
+
+    from project.api.users import users_blueprint
+    app.register_blueprint(users_blueprint)
+
+    @app.shell_context_processor
+    def ctx():
+        return {'app': app, 'db': db}
+
+    return app
--- services/users/project/tests/test_config.py
+++ services/users/project/tests/test_config.py
@@ -3,20 +3,21 @@ import unittest
 
 from flask import current_app
 from flask_testing import TestCase
-from project import app
+from project import create_app
 
 
 class TestDevelopmentConfig(TestCase):
 
     def create_app(self):
+        app = create_app()
         app.config.from_object('project.config.DevelopmentConfig')
         return app
 
     def test_app_is_development(self):
-        self.assertTrue(app.config['SECRET_KEY'] == 'my_precious')
+        self.assertTrue(self.app.config['SECRET_KEY'] == 'my_precious')
         self.assertFalse(current_app is None)
         self.assertTrue(
-            app.config['SQLALCHEMY_DATABASE_URI'] ==
+            self.app.config['SQLALCHEMY_DATABASE_URI'] ==
             os.environ.get('DATABASE_URL')
         )
 
@@ -24,27 +25,29 @@ class TestDevelopmentConfig(TestCase):
 class TestTestingConfig(TestCase):
 
     def create_app(self):
+        app = create_app()
         app.config.from_object('project.config.TestingConfig')
         return app
 
     def test_app_is_testing(self):
-        self.assertTrue(app.config['SECRET_KEY'] == 'my_precious')
-        self.assertTrue(app.config['TESTING'])
-        self.assertFalse(app.config['PRESERVE_CONTEXT_ON_EXCEPTION'])
+        self.assertTrue(self.app.config['SECRET_KEY'] == 'my_precious')
+        self.assertTrue(self.app.config['TESTING'])
+        self.assertFalse(self.app.config['PRESERVE_CONTEXT_ON_EXCEPTION'])
         self.assertTrue(
-            app.config['SQLALCHEMY_DATABASE_URI'] ==
+            self.app.config['SQLALCHEMY_DATABASE_URI'] ==
             os.getenv('DATABASE_TEST_URL')
         )
 
 
 class TestProductionConfig(TestCase):
     def create_app(self):
+        app = create_app()
         app.config.from_object('project.config.ProductionConfig')
         return app
 
     def test_app_is_production(self):
-        self.assertTrue(app.config['SECRET_KEY'] == 'my_precious')
-        self.assertFalse(app.config['TESTING'])
+        self.assertTrue(self.app.config['SECRET_KEY'] == 'my_precious')
+        self.assertFalse(self.app.config['TESTING'])
 
 
 if __name__ == '__main__':
